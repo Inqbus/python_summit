dc407e59efa76ad1e38ed745c8a68619597d3c24
volker <volker@paddy.villa.lan>
2018-09-29 22:17:37 +0200

    added embedding, renamed some files

---
+++ embed_static/static.py
@@ -0,0 +1,24 @@
+from bokeh.plotting import figure, output_file, show
+import numpy as np
+
+def root():
+    # Anzahl der Datenpunkte
+    N = 5
+    # Daten für X-Achse als lineare Abfolge zwischen 1 und 3
+    x = np.linspace(1,3,N)
+    # Daten der Y-Achse, als Sinus der x-Stützstellen
+    y = np.sin(x)
+
+    # Der Ausgabe-File und dessen Titel
+    output_file("lines.html", title="line plot example")
+
+    # Der Plot 
+    plot = figure(title="line example", x_axis_label='x', y_axis_label='y')
+    # Die Datenreihe
+    plot.line(x, y, legend="Temp.", line_width=2)
+
+    # Kommando um den Browser automatisch zu öffnen und "lines.html" anzuzeigen
+    return plot
+
+if __name__ == "__main__" :
+    show( root() )
